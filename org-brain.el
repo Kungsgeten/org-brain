cbeb9fab56a969bc15afe9ef6defe89271203137
Galen Menzel <galen.menzel@utexas.edu>
2019-07-08 01:21:58 +0200

    Display navigation history
    - Show buttons at the top of vis for the last five entries visited.
    - defcustom for whether to show history
    - History list always accepts new entries, dropping old ones when too long

--- org-brain.el
+++ org-brain.el
@@ -98,6 +98,11 @@ If 'root, only choose from file entries in `org-brain-path' (non-recursive)."
   :group 'org-brain
   :type '(boolean))
 
+(defcustom org-brain-show-history t
+  "Should the navigation history be shown in `org-brain-visualize'?"
+  :group 'org-brain
+  :type '(boolean))
+
 (defcustom org-brain-quit-after-goto nil
   "Should the *org-brain* buffer window close itself after executing a goto command?"
   :group 'org-brain
@@ -1523,6 +1528,12 @@ Unless WANDER is t, `org-brain-stop-wandering' will be run."
           (entry-pos))
       (delete-region (point-min) (point-max))
       (org-brain--vis-pinned)
+      (when (not nohistory)
+        (setq org-brain--vis-history
+              (seq-filter (lambda (elt) (not (equal elt entry))) org-brain--vis-history))
+        (setq org-brain--vis-history (seq-take org-brain--vis-history 15))
+        (push entry org-brain--vis-history))
+      (when org-brain-show-history (org-brain--vis-history))
       (if org-brain-visualizing-mind-map
           (setq entry-pos (org-brain-mind-map org-brain--vis-entry org-brain-mind-map-parent-level org-brain-mind-map-child-level))
         (insert "\n\n")
@@ -1548,11 +1559,7 @@ Unless WANDER is t, `org-brain-stop-wandering' will be run."
         (org-brain-visualize-mode))
       (goto-char entry-pos))
     (unless nofocus
-      (pop-to-buffer "*org-brain*")
-      (when (and (not nohistory)
-                 (not (equal entry (car org-brain--vis-history)))
-                 (< (length org-brain--vis-history) 15))
-        (push entry org-brain--vis-history)))))
+      (pop-to-buffer "*org-brain*"))))
 
 ;;;###autoload
 (defun org-brain-visualize-entry-at-pt ()
@@ -1792,6 +1799,15 @@ Helper function for `org-brain-visualize'."
     (org-brain-insert-visualize-button pin 'org-brain-pinned))
   (insert "\n"))
 
+(defun org-brain--vis-history ()
+  "Insert the 5 most recently visited entries
+Helper function for `org-brain-visualize'."
+  (insert "HISTORY:")
+  (dolist (entry (reverse (seq-take org-brain--vis-history 5)))
+    (insert "  ")
+    (org-brain-insert-visualize-button entry 'org-brain-pinned))
+  (insert "\n"))
+
 (defun org-brain--insert-wire (&rest strings)
   "Helper function for drawing fontified wires in the org-brain visualization buffer."
   (insert (propertize (apply 'concat strings) 'face 'org-brain-wires)))
@@ -1808,7 +1824,7 @@ Helper function for `org-brain-visualize'."
         (let ((children-links (cdr parent))
               (col-start (+ 3 max-width))
               (parent-title (org-brain-title (car parent))))
-          (org-goto-line 4)
+          (org-goto-line 5)
           (mapc
            (lambda (child)
              (picture-forward-column col-start)
@@ -1817,7 +1833,7 @@ Helper function for `org-brain-visualize'."
              (setq max-width (max max-width (current-column)))
              (newline (forward-line 1)))
            (sort children-links org-brain-visualize-sort-function))
-          (org-goto-line 4)
+          (org-goto-line 5)
           (forward-line (1- (length children-links)))
           (picture-forward-column col-start)
           (push (cons (picture-current-line)
