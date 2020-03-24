f4f0ecfe5eb06fc7fb6e25d04617a2aa82de80eb
Feng Shu <tumashu@163.com>
2020-03-24 10:50:15 +0800

    Add org-brain-return-entry
    * org-brain.el (org-brain-return-entry): New function.
    (org-brain-choose-entries): Use org-brain-return-entry.

--- org-brain.el
+++ org-brain.el
@@ -823,45 +823,49 @@ For PREDICATE, REQUIRE-MATCH, INITIAL-INPUT, HIST, DEF and INHERIT-INPUT-METOD s
          (choices (completing-read prompt targets
                                    predicate require-match initial-input hist def inherit-input-method)))
     (mapcar (lambda (title)
-              (let ((id (or (cdr (assoc title targets))
-                            title)))
-                (or
-                 ;; Headline entry exists, return it
-                 (org-brain-entry-from-id id)
-                 ;; File entry
-                 (progn
-                   (setq id (split-string id "::" t))
-                   (let* ((entry-path (org-brain-entry-path
-                                       (funcall org-brain-file-from-input-function id)
-                                       t))
-                          (entry-file (org-brain-path-entry-name entry-path)))
-                     (unless (file-exists-p entry-path)
-                       (make-directory (file-name-directory entry-path) t)
-                       (write-region "" nil entry-path))
-                     (if (or (not org-brain-include-file-entries)
-                             (equal (length id) 2)
-                             (not (equal (car id) entry-file)))
-                         ;; Create new headline entry in file
-                         (org-with-point-at (org-brain-entry-marker entry-file)
-                           (if (and (not org-brain-include-file-entries)
-                                    (or
-                                     ;; Search heading without tags
-                                     (save-excursion
-                                       (re-search-forward (concat "\n\\* +" (car id) "[ \t]*$") nil t))
-                                     ;; Search heading with tags
-                                     (save-excursion
-                                       (re-search-forward (concat "\n\\* +" (car id) "[ \t]+:.*:$") nil t))))
-                               (org-brain-entry-at-pt)
-                             (goto-char (point-max))
-                             (insert (concat "\n* " (or (cadr id) (car id))))
-                             (let ((new-id (org-id-get-create)))
-                               (run-hooks 'org-brain-new-entry-hook)
-                               (save-buffer)
-                               (list entry-file (or (cadr id) (car id)) new-id))))
-                       entry-file))))))
+              (org-brain-return-entry title targets))
             (if org-brain-entry-separator
                 (split-string choices org-brain-entry-separator)
               (list choices)))))
+
+(defun org-brain-return-entry (title targets)
+  "Search and return a entry which title is TITLE from TARGETS, create if possible."
+  (let ((id (or (cdr (assoc title targets))
+                title)))
+    (or
+     ;; Headline entry exists, return it
+     (org-brain-entry-from-id id)
+     ;; File entry
+     (progn
+       (setq id (split-string id "::" t))
+       (let* ((entry-path (org-brain-entry-path
+                           (funcall org-brain-file-from-input-function id)
+                           t))
+              (entry-file (org-brain-path-entry-name entry-path)))
+         (unless (file-exists-p entry-path)
+           (make-directory (file-name-directory entry-path) t)
+           (write-region "" nil entry-path))
+         (if (or (not org-brain-include-file-entries)
+                 (equal (length id) 2)
+                 (not (equal (car id) entry-file)))
+             ;; Create new headline entry in file
+             (org-with-point-at (org-brain-entry-marker entry-file)
+               (if (and (not org-brain-include-file-entries)
+                        (or
+                         ;; Search heading without tags
+                         (save-excursion
+                           (re-search-forward (concat "\n\\* +" (car id) "[ \t]*$") nil t))
+                         ;; Search heading with tags
+                         (save-excursion
+                           (re-search-forward (concat "\n\\* +" (car id) "[ \t]+:.*:$") nil t))))
+                   (org-brain-entry-at-pt)
+                 (goto-char (point-max))
+                 (insert (concat "\n* " (or (cadr id) (car id))))
+                 (let ((new-id (org-id-get-create)))
+                   (run-hooks 'org-brain-new-entry-hook)
+                   (save-buffer)
+                   (list entry-file (or (cadr id) (car id)) new-id))))
+           entry-file))))))
 
 (defun org-brain-choose-entry (prompt entries &optional predicate require-match initial-input hist def inherit-input-method)
   "PROMPT for an entry from ENTRIES and return it.
